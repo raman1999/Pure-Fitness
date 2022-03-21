46366a15f6d35d42f16737d9fd1d91ea1e51796b
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-03-21 01:39:59 -0700

    feat : added signup page

---
+++ src/Context/AuthenticationProvider.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+import { useState } from "react";
+
+const AuthenticationContext = createContext();
+
+export const AuthenticationProvider = ({ children }) => {
+    const [login, setLogin] = useState(false);
+
+    return (
+        <AuthenticationContext.Provider value={{ login, setLogin }}>
+            {children}
+        </AuthenticationContext.Provider>
+    );
+};
+export const useAuthenticationContext = () => useContext(AuthenticationContext);
