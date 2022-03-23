d364de5c103ac23500e8cac7ec8be1e7de5c91df
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-03-23 05:04:45 -0700

    feat : added product listing page

--- src/Components/Product/ProducListing.jsx
+++ src/Components/Product/ProducListing.jsx
@@ -1,6 +1,53 @@
-export function ProductListing() {
+import { useEffect } from "react";
+import { useFilterContext } from "../../Context";
+import { ProductFilter } from "./ProductFilter";
+import { getFilterProducts } from "../../Utils";
+import { ProductCard } from "./ProductCard";
+import "./products.css";
+import { LoadingSpinner } from "../../Utils";
+import { UseGetAxios } from "../../Hooks/UseGetAxios";
 
-    return (
-        <div>This is Product page</div>
-    )
+export function ProductListing() {
+  const { filterState, filterDispatch } = useFilterContext();
+
+  const {
+    serverData: { products },
+    isLoading,
+  } = UseGetAxios("api/products");
+
+  useEffect(() => {
+    console.log("running");
+    filterDispatch({ type: "SET_PRODUCTS", payload: products });
+  }, [products]);
+
+  const filterProducts = !isLoading
+    ? getFilterProducts(filterState, filterState.originalProducts)
+    : [];
+
+  return (
+    <>
+      {isLoading && (
+        <div className="loader">
+          <LoadingSpinner />
+        </div>
+      )}
+      <div className="grid-container">
+        {!isLoading && <ProductFilter />}
+
+        {!isLoading && (
+          <div className="products-category-box">
+            <h2 className="title txt-center txt-gray">
+              {filterProducts.length > 0 ? "All Products" : "No Results Found"}
+            </h2>
+
+            <section className="products-list flex-box">
+              {filterProducts?.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </section>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
